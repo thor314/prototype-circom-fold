17942891fbc535ce0d9bc4fd8a89431e492a0c08
Waylon Jepsen <waylonjepsen1@gmail.com>
2024-10-22 17:03:34 -0600

    compile

--- src/tests.rs
+++ src/tests.rs
@@ -2,11 +2,17 @@ use std::collections::HashMap;
 
 use log::debug;
 use proofs::program::{self, data::{CircuitData, InstructionConfig, R1CSType, SetupData, WitnessGeneratorType}};
+use serde_json::json;
 
 const ENTRY_EXTERNAL_R1CS: &[u8] = include_bytes!("entry.r1cs");
 const ENTRY_WITNESS_GENERATOR: &[u8] = include_bytes!("entry.bin");
 const JSON_MAX_ROM_LENGTH: usize = 35;
 
+const MUL_X: (&str, [u8; 16]) =
+    ("X", [0x80, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00]);
+
+const MUL_Y: (&str, [u8; 16]) =
+    ("Y", [0x80, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00]);
 
 #[test]
 fn test_setup() {   
@@ -31,9 +37,8 @@ fn test_setup() {
     let aes_rom_opcode_config = InstructionConfig {
         name:          String::from("AES_GCM_1"),
         private_input: HashMap::from([
-          (String::from(AES_KEY.0), json!(AES_KEY.1)),
-          (String::from(AES_IV.0), json!(AES_IV.1)),
-          (String::from(AES_AAD.0), json!(AES_AAD.1)),
+          (String::from(MUL_X.0), json!(MUL_X.1)),
+          (String::from(MUL_Y.0), json!(MUL_Y.1)),
         ]),
       };
 
