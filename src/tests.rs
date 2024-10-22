b9e14a60a672c9bc608eb5f78360b83eda360949
Waylon Jepsen <waylonjepsen1@gmail.com>
2024-10-22 17:12:48 -0600

    test

--- src/tests.rs
+++ src/tests.rs
@@ -1,7 +1,7 @@
 use std::collections::HashMap;
 
 use log::debug;
-use proofs::program::{self, data::{CircuitData, InstructionConfig, R1CSType, SetupData, WitnessGeneratorType}};
+use proofs::program::{self, data::{CircuitData, FoldInput, InstructionConfig, R1CSType, SetupData, WitnessGeneratorType}};
 use serde_json::json;
 
 const ENTRY_EXTERNAL_R1CS: &[u8] = include_bytes!("entry.r1cs");
@@ -34,12 +34,40 @@ fn test_setup() {
       (String::from("entry"), CircuitData { opcode: 0 }),
     ]);
 
-    let aes_rom_opcode_config = InstructionConfig {
-        name:          String::from("AES_GCM_1"),
+    let entry_rom_opcode_config = InstructionConfig {
+        name:          String::from("entry"),
         private_input: HashMap::from([
           (String::from(MUL_X.0), json!(MUL_X.1)),
           (String::from(MUL_Y.0), json!(MUL_Y.1)),
         ]),
       };
+
+      debug!("Creating `private_inputs`...");
+      let mut rom = [entry_rom_opcode_config];
+
+      let inputs = HashMap::from([(String::from("AES_GCM_1"), FoldInput {
+        value: HashMap::from([(
+          String::from(AES_PLAINTEXT.0),
+          AES_PLAINTEXT.1.iter().map(|val| json!(val)).collect::<Vec<Value>>(),
+        )]),
+      })]);
+    
+      let mut initial_nivc_input = AES_BYTES.to_vec();
+      initial_nivc_input.extend(AES_PLAINTEXT.1.iter());
+      initial_nivc_input.resize(4160, 0); // TODO: This is currently the `TOTAL_BYTES` used in circuits
+      let initial_nivc_input = initial_nivc_input.into_iter().map(u64::from).collect();
+      let program_data = ProgramData::<Online, NotExpanded> {
+        public_params,
+        setup_data,
+        rom_data,
+        rom,
+        initial_nivc_input,
+        inputs,
+        witnesses: vec![],
+      }
+      .into_expanded();
+      debug!("program_data.inputs: {:?}, {:?}", program_data.inputs.len(), program_data.inputs[15]);
+    
+      let recursive_snark = program::run(&program_data);
 
 }
